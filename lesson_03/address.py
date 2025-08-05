1e7b80c1b89ee80eed1e9b583d409ad2b0047fb8
Kos1 <k.barcelona@vk.com>
2025-08-05 15:20:38 +0300

    lesson3

---
+++ lesson_03/address.py
@@ -0,0 +1,8 @@
+class Address:
+    def __init__(self, index, city, street, house, flat):
+        print("Создан")
+        self.index = index
+        self.city = city
+        self.street = street
+        self.house = house
+        self.flat = flat
---
+++ lesson_03/mailing.py
@@ -0,0 +1,6 @@
+class Mailing:
+    def __init__(self, to_address, from_address, cost, track):
+        self.toaddress = to_address
+        self.fromaddress = from_address
+        self.cost = cost
+        self.track = track
