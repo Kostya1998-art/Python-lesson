e5018bc25c3fabb23023b40efff5b31d0555d64a
Kos1 <k.barcelona@vk.com>
2025-08-19 12:23:22 +0300

    lesson6

---
+++ lesson_06/Дождаться картинки.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as chromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(service=chromeService(ChromeDriverManager().install()
+                                                ))
+driver.implicitly_wait(15)
+driver.get
+("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+waiter = WebDriverWait(driver, 20)
+waiter.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#landscape")))
+print(driver.find_element(By.CSS_SELECTOR, "#award").get_dom_attribute("src"))
+driver.quit()
